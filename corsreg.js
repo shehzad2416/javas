69a81691da7cb2a531094469c77283b3623cd479
shehzad2416 <99542353+shehzad2416@users.noreply.github.com>
2022-04-23 17:31:07 +0500

    Add files via upload
    email wale coding

---
+++ corsreg.js
@@ -0,0 +1,31 @@
+// var english = parseInt(prompt("english marks"));
+// var computer = parseInt(prompt("computer marks"));
+// var maths = parseInt(prompt("maths marks"));
+// var sindi = parseInt(prompt("sindi marks"));
+// var chemistry = parseInt(prompt("chemistry marks"));
+// var total = english + computer + maths + sindi + chemistry
+// document.write("total" + total + "<br />")
+// var per = (total * 100) / 500;
+// document.write("per" + per +"<br />");
+// if (per >= 80) {
+//     document.write("you have got A+")
+// }
+// else if (per >= 70) {
+//     document.write("you have got A")
+// }
+// else if (per >= 60) {
+//     document.write("you have got B")
+// } else if (per >= 50) {
+//     document.write("you have got C")
+// }  
+// else  {
+//     document.write("you have got Fail")
+// }
+var Name = prompt("Enter you email");
+var Password = prompt("Enter you Password"); 
+if (Name == "shehzadahmd24@gmail.com" && Password == "5246"){
+    document.write("welcom admin")
+}
+else {
+    document.write("Invaild credentails")
+}
